2913f0efc04a9e6259fedd928b68f5a6b549e8a4
iagokrt <iago_floripa@hotmai.com>
2020-10-06 08:10:59 -0300

    initial commit for project-treh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from "react-three-fiber";
+import Lights from "./components/Lights";
+import Ambient from "./components/Ambient";
+import "./styles.css";
+
+import { OrbitControls } from 'drei';
+
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={[1.5, 1.5, 1.5]}
+    >
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={'hotpink'} />
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <Lights />
+      <Ambient />
+      <Box position={[-1.2, 0, 0]} />
+      <OrbitControls />
+    </Canvas>
   );
 }
 
